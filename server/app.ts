ce340b6673c2989efc37c961fef5004b97ffd756
steinkamp245 <lutz.steinkamp92@gmail.com>
2018-10-11 15:19:08 +0100

    Updated something port related I guess?

--- server/app.ts
+++ server/app.ts
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
     res.status(200).json({ message: 'I was changed on the feature branch!' });
 });
 
-const PORT = process.env.NODE_ENV === 'test' ? 0 : (process.env.PORT || 3000);
-const server = app.listen(80, () => console.log(`Listening on port ${80}`));
+const PORT = process.env.NODE_ENV === 'test' ? 0 : (process.env.PORT || 80);
+const server = app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 module.exports = server;
